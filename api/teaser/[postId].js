57da8e11034e2f43b37cd2430ee6dbaa9fbcc542
Philipp <56469697+Donnie-design@users.noreply.github.com>
2022-06-30 14:36:58 +0200

    add dynamic api route

---
+++ api/teaser/[postId].js
@@ -0,0 +1,14 @@
+import connectToMongodb from '../../backend/db/connect';
+import Blogposts from '../../backend/models/Blogposts';
+
+export default async function handler(request, response) {
+	const {postId} = request.query;
+
+	switch (request.method) {
+		case 'GET': {
+			const post = await Blogposts.findById(postId);
+			return response.status(200).json({data: post});
+		}
+	}
+	return response.status(403).json({message: 'Error: You shall not pass!'});
+}

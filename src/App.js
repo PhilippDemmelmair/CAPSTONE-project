8d47e5dd635d03361ebcca0281327f5876603bdc
Philipp Demmelmair <56469697+PhilippDemmelmair@users.noreply.github.com>
2022-07-03 14:50:13 +0200

    Load in teaser (#14)
    * added connection to db
    
    * fixing some linting
    
    * moved fetch function in index.js / switched from own fetch method to useFetch function / fixed bug in my model
    
    * data from db gets loaded in teasercards
    
    * fixed linting issues
    
    * fixed linting issues
    
    * one ' killed my app, so i killed the '
    
    * fixed a vercel issue
    
    * fixed an issue
    
    * trying to fix issues with vercel
    
    * vercel issues
    
    * some refactoring and the cool use of the &&-operator, thx to Jessica
    
    * had forgotten to import article
    
    * fixed linting
    
    * add dynamic api route
    
    * add connectToMongodb in api/teaser/[postId]
    
    * add Link to teaser element
    
    * add detail sites for own posts / add routing / add method for getting a single post
    
    * implement recommended refactoring
    
    Co-authored-by: Philipp <56469697+Donnie-design@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Home from './pages';
 import About from './pages/about';
+import Article from './pages/article';
 import {GlobalStyle} from './styles';
 
 export default function App() {
@@ -13,6 +14,9 @@ export default function App() {
 					<Route path="/">
 						<Route index element={<Home />} />
 						<Route path="about/" element={<About />} />
+						{/* <Route path="article/"> */}
+						<Route path=":articleId" element={<Article />} />
+						{/* </Route> */}
 					</Route>
 				</Routes>
 			</BrowserRouter>

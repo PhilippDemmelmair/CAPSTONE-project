90009664b275692089a62fa0371b2b867715e0db
Philipp Demmelmair <56469697+PhilippDemmelmair@users.noreply.github.com>
2022-07-04 14:17:32 +0200

    add admindashboard mockup (#16)
    * add admindashboard mockup
    
    * fixed linting
    
    * fixed linting
    
    Co-authored-by: Philipp <56469697+Donnie-design@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Home from './pages';
 import About from './pages/about';
+import Admin from './pages/admin';
 import Article from './pages/article';
 import {GlobalStyle} from './styles';
 
@@ -14,7 +15,7 @@ export default function App() {
 					<Route path="/">
 						<Route index element={<Home />} />
 						<Route path="about/" element={<About />} />
-						{/* <Route path="article/"> */}
+						<Route path="admin/" element={<Admin />} />
 						<Route path=":articleId" element={<Article />} />
 						{/* </Route> */}
 					</Route>

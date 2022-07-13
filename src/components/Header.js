fad842377ed23ed94667787b1c61022e63f19947
Philipp Demmelmair <56469697+PhilippDemmelmair@users.noreply.github.com>
2022-07-13 14:37:37 +0200

    Navigation (#25)
    * added a image for the header for styling
    
    * made changes to avoid merge conflicts with other branch
    
    * add navigation menu / add homebutton integrated in header image
    
    * deleted placeholder elements I forgot
    
    * fixed linting
    
    * fixing the linting
    
    * fixing the linting
    
    * fixing the linting
    
    * fixing the linting
    
    * fixed clean-code issues
    
    Co-authored-by: Philipp <56469697+Donnie-design@users.noreply.github.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,91 @@
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+import headerImg from './header.png';
 
 export default function Header() {
+	const [isMenuOpen, setisMenuOpen] = useState(false);
+	function toggleisMenuOpen() {
+		setisMenuOpen(!isMenuOpen);
+	}
+
 	return (
 		<header>
-			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/about">About me</Link>
-			</nav>
+			<Navigation>
+				<Link to="/">
+					<Logo src={headerImg}></Logo>
+				</Link>
+				<DropDown>
+					<MenuWrapper>
+						<Menu>Navigation</Menu>
+						<Down onClick={toggleisMenuOpen}>Down</Down>
+					</MenuWrapper>
+
+					{isMenuOpen && (
+						<>
+							<NavElement>
+								<Link to="/" style={{textDecoration: 'none', color: 'white'}}>
+									Home
+								</Link>
+							</NavElement>
+							<NavElement>
+								<Link to="/about" style={{textDecoration: 'none', color: 'white'}}>
+									About me
+								</Link>
+							</NavElement>
+						</>
+					)}
+				</DropDown>
+			</Navigation>
 		</header>
 	);
 }
+
+const Navigation = styled.nav``;
+
+const DropDown = styled.article`
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-between;
+	width: 60vw;
+	margin: 0 auto;
+	border: 1px solid white;
+	border-radius: 8px;
+`;
+
+const Down = styled.p`
+	width: 100%;
+	margin: auto 0;
+	color: white;
+	font-size: 1rem;
+	text-align: center;
+`;
+
+const Menu = styled.p`
+	margin: auto 0;
+	padding: 0 2vw;
+	border-right: 1px solid white;
+	color: white;
+	font-size: 2rem;
+`;
+
+const MenuWrapper = styled.section`
+	display: flex;
+	flex-flow: row nowrap;
+	width: 100%;
+`;
+
+const NavElement = styled.section`
+	width: 90%;
+	margin: 2% auto;
+	padding: 2%;
+	border: 1px solid white;
+	border-radius: 8px;
+	color: white;
+	text-align: center;
+`;
+
+const Logo = styled.img`
+	width: 100%;
+`;

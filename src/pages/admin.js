93c5eb1eecac4a33e26dc945512704290ee373e0
Philipp Demmelmair <56469697+PhilippDemmelmair@users.noreply.github.com>
2022-07-18 16:31:25 +0200

    Writing posts (#26)
    * add post method to save blogposts
    
    * delete published property to avoid merge conflict
    
    * add form to type out blogposts
    
    * add conditions to saving post
    
    * fix linting
    
    * fix linting
    
    * fix linting
    
    * fix linting
    
    * Update api/teaser/index.js
    
    Add backticks, so that template string is properly usable.
    
    Co-authored-by: Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>
    
    * Update src/pages/admin.js
    
    Co-authored-by: Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>
    
    * deleted nested logic // add requirements for input fields
    
    * fixing a typo
    
    * fix issue
    
    Co-authored-by: Philipp <56469697+Donnie-design@users.noreply.github.com>
    Co-authored-by: Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,9 +1,24 @@
+import {useState} from 'react';
 import {Helmet} from 'react-helmet';
 import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 
 export default function AdminPage() {
+	const [post, setPost] = useState({title: '', author: 'Philipp Demmelmair', text: ''});
+
+	async function addPost(post) {
+		await fetch('api/teaser', {
+			method: 'POST',
+			body: JSON.stringify(post),
+		}).then(response => {
+			if (!response.ok) {
+				return response.statusText;
+			} else {
+				return response.message;
+			}
+		});
+	}
 	return (
 		<Layout>
 			<Helmet>
@@ -14,7 +29,40 @@ export default function AdminPage() {
 				Views <br></br>Here will be displayed, how much views the blog got;
 			</ViewWidget>
 			<NewPost>
-				New Post<br></br>Here will be a form to create new posts;
+				<BlogPostEntry
+					onSubmit={event => {
+						event.preventDefault();
+						if (post.title.length >= 10 && post.text.length >= 100) {
+							addPost(post);
+							setPost({...post, title: '', text: ''});
+						} else {
+							alert(
+								'Der Titel muss mindestens zehn Zeichen und der Text mindestens 100 Zeichen lang sein.'
+							);
+						}
+					}}
+				>
+					<TitleLabel htmlFor="input-title">Title:</TitleLabel>
+					<TitleInput
+						required
+						minlength="10"
+						id="input-title"
+						type="text"
+						value={post.title}
+						onChange={event => setPost({...post, title: event.target.value})}
+					/>
+					<PostLabel htmlFor="input-text">Text:</PostLabel>
+					<PostInput
+						required
+						minlength="100"
+						id="input-text"
+						type="textarea"
+						rows={27}
+						value={post.text}
+						onChange={event => setPost({...post, text: event.target.value})}
+					/>
+					<SaveButton>Save</SaveButton>
+				</BlogPostEntry>
 			</NewPost>
 			<Comments>
 				Comments<br></br>Here will be the section, where you can approve or dissmiss a
@@ -43,4 +91,51 @@ const Comments = styled.article`
 	margin: 2%;
 	border: 1px solid white;
 	color: white;
+`;
+
+const BlogPostEntry = styled.form``;
+
+const TitleLabel = styled.label`
+	display: block;
+	width: 40%;
+	margin: 2% auto;
+	padding: 1%;
+	border-radius: 8px;
+	background: #0496ff;
+	font-size: 1.3rem;
+	text-align: center;
+`;
+
+const TitleInput = styled.input`
+	width: 98%;
+	margin: 1% 1%;
+`;
+
+const PostLabel = styled.label`
+	display: block;
+	width: 40%;
+	margin: 2% auto;
+	padding: 1%;
+	border-radius: 8px;
+	background: #0496ff;
+	font-size: 1.3rem;
+	text-align: center;
+`;
+
+const PostInput = styled.textarea`
+	width: 98%;
+	margin: 1% 1%;
+`;
+
+const SaveButton = styled.button`
+	display: block;
+	width: 90%;
+	margin: 2% auto;
+	padding: 2%;
+	border: none;
+	border-radius: 8px;
+	background: #006ba6;
+	color: white;
+	font-size: 1.3rem;
+	text-align: center;
 `;
